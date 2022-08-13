3b1ae3c67ffcff1390bc5ab7dfecc2aa8fe35a57
Yuka <okumurahomepage@gmail.com>
2022-08-13 16:27:16 +0900

    Header complete

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,26 +6,67 @@
   <link rel="stylesheet" href="{{ url('css/style.css ')}}">
 </head>
 <body>
-  <div class="container">
-    <h1>投稿一覧</h1>
-    @guest
-      <a href="{{ route('login') }}">ログイン</a>
-      <a href="{{ route('register') }}">新規登録</a>
-    @else
-      <textarea name=""></textarea>
-      <button>投稿する</button>
-      <form action="{{ route('logout') }}" method="post">
-          @csrf
-          <input type="submit" value="ログアウト">
-      </form>
-    @endguest
+  <header>
+    <div class="container">
+      <h1>ららべる SNS</h1>
+        <ul>
+          @guest
+            <!-- <a href="{{ route('login') }}">ログイン</a>
+            <a href="{{ route('register') }}">新規登録</a> -->
+            <li>
+              <button onclick="location.href='{{ route('login') }}'">ログイン</button>
+            </li>
+            <li>
+              <button onclick="location.href='{{ route('register') }}'">新規登録</button>
+            </li>
+          @else
+            <li>
+              <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <input type="submit" value="ログアウト">
+              </form>
+            </li>
+          @endguest
+        </ul>
+    </div>
+  </header>
 
-    <ul>
-      <li>
-        <p>ユーザー1 2022-08-11 21:12</p>
-        <p>初投稿です！</p>
-      </li>
-    </ul>
-  </div>
+  <main>
+    <div class="container">
+      @auth
+        <div class="form-post">
+          <textarea name=""></textarea>
+          <button>投稿する</button>
+        </div>
+      @endauth
+      <h2>投稿一覧</h2>
+      <ul>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+        <li>
+          <p>ユーザー1 2022-08-11 21:12</p>
+          <p>初投稿です！</p>
+        </li>
+      </ul>
+    </div>
+  </main>
 </body>
 </html>
